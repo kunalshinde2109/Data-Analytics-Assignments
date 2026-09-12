ffe35d01cbddb26d74c09cef743121563415ae50
Kunal Shinde <kunalshinde1600@gmail.com>
2026-09-12 15:04:53 +0530

    Delete calculate area of rectangle.py

--- calculate area of rectangle.py
+++
@@ -1,7 +0,0 @@
-#Write a program to calculate area of rectangle based on length and breadth. ha program de python chach
-length = int(input("Enter length: "))
-breadth = int(input("Enter breadth: "))
-
-area = length * breadth
-
-print("Area of Rectangle =", area)
