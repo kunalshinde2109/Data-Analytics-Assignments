117df7e636288e4ccf8059fd9a9dcf04246a0321
Kunal Shinde <kunalshinde1600@gmail.com>
2026-09-12 15:06:22 +0530

    Delete third angle traingle.py

--- third angle traingle.py
+++
@@ -1,9 +0,0 @@
-#Write a Program to input two angles from user and find third angle of the triangle.
-angle1 = int(input("Enter first angle: "))
-angle2 = int(input("Enter second angle: "))
-
-angle3 = 180 - (angle1 + angle2)
-
-print("First Angle =", angle1)
-print("Second Angle =", angle2)
-print("Third Angle =", angle3)
